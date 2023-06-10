d48885f67dccd4450cafe22b2f8982928355dd83
GiuntiniF <francesco.giuntini2@stud.unifi.it>
2023-06-10 16:32:50 +0200

    deploy railway

--- task_manager_api/swagger.py
+++ task_manager_api/swagger.py
@@ -2,17 +2,25 @@ from django.urls import path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from pathlib import Path
+import environ
+import os
 
+env = environ.Env(
+    DEBUG=(bool, False),
+    SECRET_KEY=(str)
+)
 
 schema_view = get_schema_view(
     openapi.Info(
         title="Your API",
         default_version='v1',
         description="API documentation",
-        # terms_of_service="",
-        # contact=openapi.Contact(email="contact@example.com"),
-        # license=openapi.License(name="BSD License"),
+        terms_of_service="",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
     ),
+    url=env("API_URL"),
     public=True,
     permission_classes=(permissions.AllowAny,))
 
