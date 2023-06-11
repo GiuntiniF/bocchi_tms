7d7f040e82048c0912a9ba4e4e32ec32be92b99b
GiuntiniF <francesco.giuntini2@stud.unifi.it>
2023-06-11 12:07:41 +0200

    Final Commit...?

--- bocchi_tms/asgi.py
+++ bocchi_tms/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for task_manager_api project.
+ASGI config for bocchi_tms project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- bocchi_tms/wsgi.py
+++ bocchi_tms/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for task_manager_api project.
+WSGI config for bocchi_tms project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
